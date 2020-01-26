482d142a52becd38dd057e43a8d8bf96a9a3ad97
mccarow <47663134+mccarow@users.noreply.github.com>
2020-01-26 02:51:25 +0100

    Add files via upload
    *with actual prices
    *without empty lines

--- Bakery.php
+++ Bakery.php
@@ -1,8 +1,6 @@
 <?php
-
-function calc($packs)
+function calc($packs, $prices, $xyz)
 {
-	$xyz=21;
 	$s=0;
 	$m=0;
 	$l=0;
@@ -18,23 +16,32 @@ function calc($packs)
 	}
 
 	$l=$xyz / $packs[2];
-$total=$l * 24.95 + $m * 16.95 + $s * 9.95;
+$total=$l * $prices[2] + $m * $prices[1] + $s * $prices[0];
 
 	echo $xyz + $m*$packs[1] + $s*$packs[0] . " " . 'MB11' ." ". "$" . $total;
 	echo "</br>";
-	echo "&nbsp;&nbsp;&nbsp;&nbsp" . "$l x " .$packs[2]. "$24.95 </br>";
-	echo "&nbsp;&nbsp;&nbsp;&nbsp" . "$m x " .$packs[1]. "$16.95 </br>";
-	echo "&nbsp;&nbsp;&nbsp;&nbsp" . "$s x " .$packs[0]. "$9.95</br></br>";
-	
+	if ($l>0) {
+	  echo "&nbsp;&nbsp;&nbsp;&nbsp" . $l . " x " . $packs[2]. " " . $prices[2] . "</br>";
+	}
+	if ($m>0) {
+		echo "&nbsp;&nbsp;&nbsp;&nbsp" . $m . " x " . $packs[1]. " " . $prices[1] . "</br>";
+	}
+	if ($s>0) {
+		echo "&nbsp;&nbsp;&nbsp;&nbsp" . $s . " x " . $packs[0]. " " . $prices[0] . "</br></br>";
+	}
 }
 //Vegemite Scroll
 
 //Blueberry Muffin
+$xyz=15;
 $packs = array(2, 5, 8);
-calc($packs);
+$prices = array(9.95, 16.95, 24.95);
+calc($packs, $prices,$xyz);
 //Croissant
+$xyz=21;
 $packs = array(3, 5, 9);
-calc($packs);
+$prices = array(5.95, 9.95, 16.99);
+calc($packs, $prices,$xyz);
 
 /*
 1.prices
